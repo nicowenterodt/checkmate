2826ca2f6f9417512048e049b33242ff68c272db
Nico Wenterodt <nico@niwen.de>
2023-08-19 16:29:13 +0200

    use scope for basepath

--- next.config.js
+++ next.config.js
@@ -3,10 +3,9 @@ module.exports = {
 };
 const withPWA = require("next-pwa");
 module.exports = withPWA({
-
   dest: "public",
   register: true,
   disable: process.env.NODE_ENV === 'development',
   skipWaiting: true,
-  basePath: '/github-pages'
+  scope: '/github-pages'
 });
