d9ecb7872a9e990a8c6824df3a2e2d368a7288bc
Nico Wenterodt <nico@niwen.de>
2023-08-20 11:34:59 +0200

    fix base font color and scopes

--- next.config.js
+++ next.config.js
@@ -1,12 +1,10 @@
 module.exports = {
   reactStrictMode: true,
-  basePath: '/checkmate'
 };
 const withPWA = require("next-pwa");
 module.exports = withPWA({
   dest: "public",
   register: true,
   disable: process.env.NODE_ENV === 'development',
-  skipWaiting: true,
-  scope: '/checkmate'
+  skipWaiting: true
 });
