1e9676f89c787c37dcde468993b3b4bd650fabf5
Nico Wenterodt <nico@niwen.de>
2023-09-17 11:58:46 +0200

    add missing key prop

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -10,7 +10,7 @@ const Home: NextPage = () => {
 
   return <div>
     {checkgroups.map((checkgroup) => (
-      <Link href={`/checkgroup/${checkgroup.key}`}>
+      <Link href={`/checkgroup/${checkgroup.key}`} key={checkgroup.key}>
         <div key={checkgroup.key} className={`my-1 w-full px-4 py-2.5 bg-slate-100 rounded-xl shadow-sm flex items-start text-xl`}>
           {checkgroup.name}
         </div>
